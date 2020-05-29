83e05e10d9d2cdb4ed30c7f06333d4b8d6e6e556
Yurisbel Jimenez <yurisbeljimenez@gmail.com>
2020-05-29 08:27:26 -0400

    Added the auto start functionality

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ mkdir build
 cd build
 cmake --DCMAKE_CXX_FLAGS="-O3" --DCMAKE_C_FLAGS="-O3" ..
 make
+cd ~/
+echo 'if [ "x${SSH_TTY}" = "x" ]; then' >> .bashrc
+echo '  /home/pi/RPiPlay/build/rpiplay -l -n Exercise # Or wherever the rpiplay binary is located' >> .bashrc
+echo 'fi' >> .bashrc
