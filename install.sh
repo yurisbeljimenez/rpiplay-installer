3c0d412e555b4ba5b25ce34cfc2d6a23d0dc19f1
Yurisbel Jimenez <yurisbeljimenez@gmail.com>
2020-05-28 20:14:34 -0400

    Initial install

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade -y
+git clone https://github.com/FD-/RPiPlay.git
+cd RPiPlay
+sudo apt-get install cmake -y
+sudo apt-get install libavahi-compat-libdnssd-dev -y
+sudo apt-get install libplist-dev -y
+sudo apt-get install libssl-dev -y
+mkdir build
+cd build
+cmake --DCMAKE_CXX_FLAGS="-O3" --DCMAKE_C_FLAGS="-O3" ..
+make
